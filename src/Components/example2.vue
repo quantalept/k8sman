d0819d2610ba9ea5659359188a41611c1d452b0d
laptop <antony.richard@quantalept.com>
2023-05-29 21:52:58 +0530

    kube client file  added

--- src/Components/example2.vue
+++ src/Components/example2.vue
@@ -1,41 +1,29 @@
 <template>
   <div>
-    <h2>List of Pods</h2>
     <ul>
-      <li v-for="pod in pods" :key="pod.metadata.name">
-        {{ pod.metadata.name }}
-      </li>
+      <li v-for="pod in pods" :key="pod.metadata.name">{{ pod.metadata.name }}</li>
     </ul>
   </div>
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
-import { Client, config } from '@kubernetes/client-node';
+import { listPods } from '../k8s/kubernetes';
 
 export default {
-  name: 'KubernetesPodList',
-  setup() {
-    const pods = ref([]);
-
-    onMounted(async () => {
-      try {
-        const kc = new config.KubeConfig();
-        kc.loadFromDefault();
-
-        const k8sClient = new Client({ config: kc });
-
-        const response = await k8sClient.api.v1.namespaces('default').pods.get();
-        console.log('Response from Kubernetes cluster:', response.body.items);
-        pods.value = response.body.items;
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    });
-
+  data() {
     return {
-      pods,
+      pods: []
     };
   },
+  mounted() {
+    this.listPods();
+  },
+  methods: {
+    async listPods() {
+      console.log('Fetching pods...');
+      this.pods = await fetchPods();
+      console.log('Fetched pods:', this.pods);
+    }
+  }
 };
 </script>
